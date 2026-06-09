756ffe6c25d47e0ce9a6e778e0dd14e59cf658b0
Adriel Cezar <adrielcezar022@gmail.com>
2026-06-09 12:37:17 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/nelioalves/workshopmongo/WorkshopmongoApplication.java
@@ -1,7 +1,11 @@
 package com.nelioalves.workshopmongo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class WorkshopmongoApplication {
@@ -10,4 +14,12 @@ public class WorkshopmongoApplication {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
 
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void logDatabaseName() {
+		String dbName = mongoTemplate.getDb().getName();
+		System.out.println("Banco de dados ativo: " + dbName);
+	}
 }
